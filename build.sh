8b74fb96905e0b05325c5b7fc587c79355c543a6
rickonono3 <rickonono3@qq.com>
2022-08-31 00:35:16 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 127
+
+docker build -t rickonono3/consulize:rc . &
+
+rm -rf build.bak 2>/dev/null || true
+mv build build.bak 2>/dev/null || true
+rm -rf build 2>/dev/null || true
+mkdir build
+
+cp ./run-consulize.sh ./build/
+
+go get
+go build -o ./build/consulize .
+zip -r ./build/consulize_linux_amd64.zip ./build/*
